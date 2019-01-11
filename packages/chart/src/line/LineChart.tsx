66abd75116f1a914d239fe63ceb7ee9b270958ca
garfieldduck <garfieldduck@gmail.com>
2019-01-11 18:57:40 +0800

    Integrate ResponsiveLayer into LineChart

--- packages/chart/src/line/LineChart.tsx
+++ packages/chart/src/line/LineChart.tsx
@@ -1,13 +1,28 @@
+import { ResponsiveLayer } from '@ichef/transcharts-graph';
 import * as React from 'react';
 
 export interface LineChartProps {
-  framework?: string;
+  margin?: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  };
 }
 
 // #TODO: sample data
 
 export class LineChart extends React.Component<LineChartProps, {}> {
     public render() {
-        return <h1>Line Chart {this.props.framework}!</h1>;
+        return (
+          <ResponsiveLayer>
+            {({ width, height }) => (
+              <div>
+                <p>Width: {width}</p>
+                <p>Height: {height}</p>
+              </div>
+            )}
+          </ResponsiveLayer>
+        );
     }
 }
