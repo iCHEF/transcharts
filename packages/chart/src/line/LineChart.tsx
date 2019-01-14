decea1c563b5581673644786a7b2da8fa5c9ab84
garfieldduck <garfieldduck@gmail.com>
2019-01-14 16:42:47 +0800

    Add the basic props for the line chart

--- packages/chart/src/line/LineChart.tsx
+++ packages/chart/src/line/LineChart.tsx
@@ -1,6 +1,14 @@
 import { ResponsiveLayer } from '@ichef/transcharts-graph';
 import * as React from 'react';
 
+export interface Scale {
+  type: 'point' | 'time' | 'linear';
+}
+export interface Field {
+  name: string;
+  color: string;
+}
+
 export interface LineChartProps {
   margin?: {
     top: number;
@@ -8,21 +16,24 @@ export interface LineChartProps {
     bottom: number;
     left: number;
   };
+  data: object[];
+  scaleX: Scale;
+  scaleY: Scale;
+  fieldX: Field;
+  fieldsY: Field[];
 }
 
-// #TODO: sample data
-
 export class LineChart extends React.Component<LineChartProps, {}> {
-    public render() {
-        return (
-          <ResponsiveLayer>
-            {({ width, height }) => (
-              <div>
-                <p>Width: {width}</p>
-                <p>Height: {height}</p>
-              </div>
-            )}
-          </ResponsiveLayer>
-        );
-    }
+  public render() {
+    return (
+      <ResponsiveLayer>
+        {({ width, height }) => (
+          <div>
+            <p>Width: {width}</p>
+            <p>Height: {height}</p>
+          </div>
+        )}
+      </ResponsiveLayer>
+    );
+  }
 }
