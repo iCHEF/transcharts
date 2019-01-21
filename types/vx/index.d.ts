9c4bc5ff8b7ba3274ab4e054064465bcf5d6b980
garfieldduck <garfieldduck@gmail.com>
2019-01-21 12:42:37 +0800

    Add LinePath's type definition

---
+++ types/vx/index.d.ts
@@ -0,0 +1,14 @@
+declare module '@vx/shape' {
+
+  interface LinePath {
+    innerRef?: HTMLInputElement;
+    data: object[];
+    curve?: any;
+    defined?: any;
+    x: ((d: any) => any) | number;
+    y: ((d: any) => any) | number;
+    stroke?: string;
+    strokeWidth?: number;
+  }
+  const LinePath: React.ComponentType<LinePath>;
+}
