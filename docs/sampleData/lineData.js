7c00d05eb78d577ac30c67b66e7107d1c6eb79ec
garfieldduck <garfieldduck@gmail.com>
2019-01-22 16:07:06 +0800

    Refine the documentations

--- docs/sampleData/lineData.js
+++ docs/sampleData/lineData.js
@@ -1,9 +1,9 @@
 const lineData = [
-  { x: 0, y: 9, c: '2018-12-10' },
-  { x: 1, y: 5, c: '2018-12-11' },
-  { x: 2, y: 5, c: '2018-12-12' },
-  { x: 3, y: 3, c: '2018-12-13' },
-  { x: 4, y: 1, c: '2018-12-14' },
+  { x: 0, y: 9, date: '2018-12-10', weekday: 'Mon' },
+  { x: 1, y: 5, date: '2018-12-11', weekday: 'Tue' },
+  { x: 2, y: 5, date: '2018-12-12', weekday: 'Wed' },
+  { x: 3, y: 3, date: '2018-12-13', weekday: 'Thu'},
+  { x: 4, y: 1, date: '2018-12-14', weekday: 'Fri' },
 ];
 
 export default lineData;
