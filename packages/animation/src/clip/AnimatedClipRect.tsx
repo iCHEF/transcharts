bd0c79d0a8821665fa8eedc65c7adec51e2d20ee
Mika Wang <garfieldduck@gmail.com>
2019-02-14 18:02:51 +0800

    Add a basic clipped children using an unique ID

--- packages/animation/src/clip/AnimatedClipRect.tsx
+++ packages/animation/src/clip/AnimatedClipRect.tsx
@@ -1,10 +1,12 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import shortid from 'shortid';
 
 export interface AnimatedClipRectProps {
   x: number;
   y: number;
   width: number;
   height: number;
+  children: React.ReactNode;
 }
 
 export const AnimatedClipRect: FunctionComponent<AnimatedClipRectProps> = ({
@@ -12,9 +14,19 @@ export const AnimatedClipRect: FunctionComponent<AnimatedClipRectProps> = ({
   y = 0,
   width = 1,
   height = 1,
+  children,
   ...restProps
 }) => {
+  const [clipId] = useState(`clip-${shortid.generate()}`);
+
   return (
-    <rect x={x} y={y} width={width} height={height} {...restProps} />
+    <>
+      <clipPath id={clipId}>
+        <rect x={x} y={y} width={width} height={height} {...restProps} />
+      </clipPath>
+      <g clip-path={`url(#${clipId})`}>
+        {children}
+      </g>
+    </>
   );
 };
