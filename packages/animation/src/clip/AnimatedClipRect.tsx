34f2011dfe7a5953f7ade48d186bf065c2e1a2f1
Mika Wang <garfieldduck@gmail.com>
2019-02-15 11:30:28 +0800

    Add react-spring to AnimatedClipRect

--- packages/animation/src/clip/AnimatedClipRect.tsx
+++ packages/animation/src/clip/AnimatedClipRect.tsx
@@ -1,28 +1,97 @@
 import React, { FunctionComponent, useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import shortid from 'shortid';
 
 export interface AnimatedClipRectProps {
-  x: number;
-  y: number;
+  type: 'slideRight' | 'slideLeft' | 'slideUp'| 'slideDown';
   width: number;
   height: number;
+  duration: number;
   children: React.ReactNode;
 }
 
+/**
+ * Returns the animated properties for the rectangle clip path
+ */
+function getSpringConfig(
+  type: AnimatedClipRectProps['type'],
+  width: AnimatedClipRectProps['width'],
+  height: AnimatedClipRectProps['height'],
+) {
+  const results = {
+    to: {
+      width,
+      height,
+      left: 0,
+      top: 0,
+    },
+  };
+
+  switch (type) {
+    case 'slideLeft': {
+      return {
+        ...results,
+        from: {
+          height,
+          width: 0,
+          left: width,
+          top: 0,
+        },
+      };
+    }
+    case 'slideUp': {
+      return {
+        ...results,
+        from: {
+          width,
+          height: 0,
+          left: 0,
+          top: height,
+        },
+      };
+    }
+    case 'slideDown': {
+      return {
+        ...results,
+        from: {
+          width,
+          height: 0,
+          left: 0,
+          top: 0,
+        },
+      };
+    }
+    case 'slideRight':
+    default: {
+      return {
+        ...results,
+        from: {
+          height,
+          width: 0,
+          left: 0,
+          top: 0,
+        },
+      };
+    }
+  }
+}
+
 export const AnimatedClipRect: FunctionComponent<AnimatedClipRectProps> = ({
-  x = 0,
-  y = 0,
+  type,
   width = 1,
   height = 1,
+  duration = 300,
   children,
   ...restProps
 }) => {
   const [clipId] = useState(`clip-${shortid.generate()}`);
+  const springProps = useSpring(getSpringConfig(type, width, height));
+  const { left, top, width: springWidth, height: springHeight } = springProps;
 
   return (
     <>
       <clipPath id={clipId}>
-        <rect x={x} y={y} width={width} height={height} {...restProps} />
+        <animated.rect x={left} y={top} width={springWidth} height={springHeight} {...restProps} />
       </clipPath>
       <g clip-path={`url(#${clipId})`}>
         {children}
