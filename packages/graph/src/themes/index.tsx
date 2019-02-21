34b2306525c4f7a137e653966defb16908121f74
chenesan <pipio1994@gmail.com>
2019-02-21 15:41:31 +0800

    Rewrite ThemeProvider

--- packages/graph/src/themes/index.tsx
+++ packages/graph/src/themes/index.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import { schemeCategory10 } from 'd3-scale-chromatic';
 import { Theme } from '../common/types';
 
+interface ThemeProviderProps {
+    theme: Theme;
+    children: React.ReactNode;
+}
+
 export const themes = {
     default: {
         colors: {
@@ -14,7 +19,11 @@ const defaultTheme = themes.default;
 
 const { Consumer, Provider } = React.createContext(defaultTheme);
 
-export const ThemeProvider = Provider;
+export const ThemeProvider = ({ theme, children }: ThemeProviderProps) => {
+    return <Provider value={theme}>
+        {children}
+    </Provider>
+};
 export function withChartTheme<P> (WrappedComponent: React.SFC<P>) {
     const Wrapper =  (props: P) => {
         return <Consumer>
