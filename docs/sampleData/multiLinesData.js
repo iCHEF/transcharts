77245f760deb8fd9e8b13006cb80d24f98891508
chenesan <pipio1994@gmail.com>
2019-02-26 18:32:43 +0800

    Add quantitative example for multi lines

--- docs/sampleData/multiLinesData.js
+++ docs/sampleData/multiLinesData.js
@@ -4,6 +4,11 @@ export const mutliLinesData = [
   { x: 2, y: 5, type: "type2", date: '2019/01/23 00:00:00', weekday: 'Wed' },
   { x: 3, y: 3, type: "type1", date: '2019/01/24 00:00:00', weekday: 'Thu'},
   { x: 4, y: 1, type: "type2", date: '2019/01/25 00:00:00', weekday: 'Fri' },
+  { x: 10, y: 9, type: "type1", date: '2019/01/21 00:00:00', weekday: 'Mon' },
+  { x: 6, y: 5, type: "type2", date: '2019/01/22 00:00:00', weekday: 'Tue' },
+  { x: 7, y: 5, type: "type2", date: '2019/01/23 00:00:00', weekday: 'Wed' },
+  { x: 2, y: 3, type: "type1", date: '2019/01/24 00:00:00', weekday: 'Thu'},
+  { x: 8, y: 1, type: "type2", date: '2019/01/25 00:00:00', weekday: 'Fri' },
 ];
 
 export default mutliLinesData;
