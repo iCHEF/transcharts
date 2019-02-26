9c3995931417c3a594dc1c387ffee69267b08ff1
chenesan <pipio1994@gmail.com>
2019-02-26 14:03:09 +0800

    Add sequential colors in theme

--- packages/graph/src/common/types.ts
+++ packages/graph/src/common/types.ts
@@ -65,6 +65,8 @@ export interface AxisEncoding extends Encoding {
   scale?: AxisScaleType;
 }
 
+export type ColorEncoding = Encoding;
+
 export interface Margin {
   top: number;
   right: number;
@@ -88,6 +90,10 @@ export interface Theme {
   colors: {
     /** colors used for nominal data */
     category: ReadonlyArray<string>;
+    sequential: {
+      scheme: ReadonlyArray<string>;
+      interpolator: (val: number) => string;
+    }
   };
   /** x-axis theme config */
   xAxis: AxisTheme;
