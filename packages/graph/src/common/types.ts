66c6a29e0348fe3f2b1f135666f17697b48458ae
chenesan <pipio1994@gmail.com>
2019-02-26 11:11:21 +0800

    Fix linter issues in graph packages

--- packages/graph/src/common/types.ts
+++ packages/graph/src/common/types.ts
@@ -4,7 +4,6 @@ import {
   ScaleTime,
 } from 'd3-scale';
 
-
 export interface FieldSelector {
   getOriginalVal: (record: object) => any;
   getScaledVal: (record: object) => any;
@@ -18,7 +17,7 @@ export interface GraphDimension {
 
 export interface Scale {
   /** d3's Scaling function employed in specific channel */
-  scale: Function;
+  scale: (...values: any[]) => any;
 
   /** scale type string */
   scaleType: string;
@@ -67,7 +66,7 @@ export interface Encoding {
 
 export interface AxisEncoding extends Encoding {
   scale?: AxisScaleType;
-};
+}
 
 export interface Margin {
   top: number;
