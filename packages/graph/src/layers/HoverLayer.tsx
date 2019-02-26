fa2619742d79c28567cc97d94b6c06b4e1e450b2
chenesan <pipio1994@gmail.com>
2019-02-26 16:52:39 +0800

    Fix type in HoverLayer

--- packages/graph/src/layers/HoverLayer.tsx
+++ packages/graph/src/layers/HoverLayer.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import { throttle } from 'lodash-es';
 import { localPoint } from '@vx/event';
 
-import { DataLayerRenderParams } from './DataLayer';
-
 export interface HoverLayerProps {
   /** Set the information related to hover or touch interactions  */
-  setHoveredPosAndIndex: DataLayerRenderParams['setHoveredPosAndIndex'];
+  setHoveredPosAndIndex: (hoveredIndex: number, xPos: number, yPos: number) => void;
 
   /** Function to be called before the latest tooltip position is set */
   handleHover: () => void;
