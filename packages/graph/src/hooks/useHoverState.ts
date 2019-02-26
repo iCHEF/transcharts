fdef03c4c510f3bec5cab7212b4b55600863be3b
chenesan <pipio1994@gmail.com>
2019-02-26 16:19:28 +0800

    Split hoverState into 2 useState

--- packages/graph/src/hooks/useHoverState.ts
+++ packages/graph/src/hooks/useHoverState.ts
@@ -1,52 +1,53 @@
 import { useState } from 'react';
 
+export type HoveringState = boolean;
+export interface HoveredPointState {
+  /** The index of data being hovered or touched */
+  index: number;
+
+  /** The mouse hovered or touched position */
+  position: {
+    x: number;
+    y: number;
+  };
+};
+
 export interface HoverState {
   /** Records whether there is mouse or touch event generating from the inner layer */
   hovering: boolean;
 
   /** The position of the point being hovered and its mapped index of data  */
-  hoveredPoint: {
-    /** The index of data being hovered or touched */
-    index: number;
-
-    /** The mouse hovered or touched position */
-    position: {
-      x: number;
-      y: number;
-    };
-  };
+  hoveredPoint: HoveredPointState
 }
 
 export function useHoverState() {
-  const [hoverState, setState] = useState({
-    hovering: false,
-    hoveredPoint: {
-      index: 0,
-      position: {
-        x: 0,
-        y: 0,
-      },
+  const [hovering, setHovering] = useState(false);
+  const [hoveredPoint, setHoveredPoint] = useState({
+    index: 0,
+    position: {
+      x: 0,
+      y: 0,
     },
   });
 
   const clearHovering = () => {
-    setState({ ...hoverState, hovering: false });
+    setHovering(false);
   };
 
   const setHoveredPosAndIndex = (hoveredIndex: number, xPos: number, yPos: number) => {
-    setState({
-      hovering: true,
-      hoveredPoint: {
-        index: hoveredIndex,
-        position: {
-          x: xPos,
-          y: yPos,
-        },
+    setHovering(true);
+    setHoveredPoint({
+      index: hoveredIndex,
+      position: {
+        x: xPos,
+        y: yPos,
       },
     });
   };
+
   return {
-    ...hoverState,
+    hovering,
+    hoveredPoint,
     clearHovering,
     setHoveredPosAndIndex,
   };
