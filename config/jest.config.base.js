64ab791895d6418ca2b58f85418ece8382038cab
chenesan <pipio1994@gmail.com>
2019-03-06 15:27:19 +0800

    Setup enzyme

--- config/jest.config.base.js
+++ config/jest.config.base.js
@@ -1,9 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/src/tsconfig.json',
     },
   },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
 };
